be98b376c70d8f7f6183f7440c18a0d099675f5c
chen-qirong <161271993+chen-qirong@users.noreply.github.com>
2024-02-26 20:08:20 +0800

    Add files via upload

---
+++ 二分法.cpp
@@ -0,0 +1,32 @@
+public int binarySearch(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+        return -1;
+    }
+    
+    int left = 0;
+    int right = nums.length - 1;
+    
+    while (left + 1 < right) {
+        int mid = left + (right - left) / 2;
+        
+        if (nums[mid] == target) {
+            right = mid;
+        } else if (nums[mid] < target) {
+            left = mid;
+            // or left = mid + 1;
+        } else {
+            right = mid;
+            // or right = mid -1;
+        }
+    }
+    
+    if (nums[left] == target) {
+        return left;
+    }
+    
+    if (nums[right] == target) {
+        return right;
+    }
+    
+    return -1;
+}
